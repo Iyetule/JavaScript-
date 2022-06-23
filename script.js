c90eb9504c58c89a6bd50b669dd1d351f542dedc
Iyetule <105356802+Iyetule@users.noreply.github.com>
2022-06-23 01:00:08 +0100

    Simple Arithmetic calculator

---
+++ script.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator</title>
+    
+</head>
+<body>
+    <script type="text/javascript">
+        var a,b,add,sub,mul,div;
+        a=parseInt(prompt("Enter first value"));
+        b=parseInt(prompt("Enter second value"));
+        add=a+b;
+        sub=a-b;
+        mul=a*b;
+        div=a/b;
+        alert("Add Result is"+ add);
+        alert("Sub Result is"+sub);
+        alert("Mul Result is"+mul);
+        alert("Div Result is"+div);
+
+    </script>
+</body>
+</html>
